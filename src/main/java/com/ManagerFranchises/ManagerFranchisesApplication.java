44ada7a147b6bf37386fd6dd40079013bad6a6e0
evalenci1981 <evalencia1981@gmail.com>
2024-09-30 09:10:18 -0500

    se cambia ruta del archivo aplicación y la estructura, se cambia nombre carpeta por minuscula por que no estaba compilando

--- src/main/java/com/ManagerFranchises/ManagerFranchisesApplication.java
+++ src/main/java/com/ManagerFranchises/ManagerFranchisesApplication.java
@@ -1,4 +1,4 @@
-package com.ManagerFranchises.ManagerFranchises;
+package com.managerfranchises;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
