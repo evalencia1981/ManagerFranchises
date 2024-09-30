f5faa72725cca2e8e2359591e27c0772093aa2ec
evalenci1981 <evalencia1981@gmail.com>
2024-09-30 00:00:29 -0500

    proyecto inicial master o trunk, compila correctamente

---
+++ src/main/java/com/ManagerFranchises/ManagerFranchises/ManagerFranchisesApplication.java
@@ -0,0 +1,13 @@
+package com.ManagerFranchises.ManagerFranchises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerFranchisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerFranchisesApplication.class, args);
+	}
+
+}
